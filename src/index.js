4344be29bad831e72660b754add10892e22ec569
Imran Sahil <imdshizzle@gmail.com>
2020-04-08 23:06:00 -0400

    bugfix/update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import '../src/index.css';
 import App from '../src/App';
 import * as serviceWorker from '../src/serviceWorker';
 
 ReactDOM.render(
-    <App />,
+  <BrowserRouter basename={'/src'}>
+    <App exact path="/" component={App} />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
